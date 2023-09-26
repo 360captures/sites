28ea9b67c61889fcbab68df84470a79baf1cc78e
fvacreate <78551936+vladislavfva@users.noreply.github.com>
2023-09-26 21:16:02 +0200

    Update and rename gallery.js to gallery_v1.0.0.js

---
+++ gallery_v1.0.0.js
@@ -0,0 +1,85 @@
+var bgSwiper;
+var thumbsSwiper;
+
+// Функция для создания Swiper
+function initSwiper() {
+    $('.t1--slider__content').each(function(index) {
+      bgSwiper = new Swiper($(this).find('.swiper.t1--gallery__slider')[0], {
+        slidesPerView: "auto",
+        speed: 1000,
+        allowTouchMove: false,
+      });
+    });
+}
+  
+function initThumbs() {
+    $('.t1--slider__content').each(function(index) {
+        thumbsSwiper = new Swiper($(this).find('.swiper.t1--gallery__thumbs')[0], {
+            allowTouchMove: true,
+            speed: 1000,
+            slidesPerView: 'auto',
+            slideToClickedSlide: true,
+            centeredSlides: true,
+            spaceBetween: 16,
+            mousewheel: {
+            forceToAxis: true,
+            },
+            thumbs: {
+            swiper: bgSwiper,
+            },
+            navigation: {
+            nextEl: '.t1--gallery__next',
+            prevEl: '.t1--gallery__prev',
+            },
+        });
+    });
+}
+
+$(document).ready(function(){
+    function initGallery() {
+        initSwiper();
+        initThumbs();
+    }
+
+    function countSlides() {
+        let totalSlides = $('.t1--slider__content').find('.swiper-slide.t1--gallery__image').length;
+        $('.t1--open-lightbox').text("View All " + totalSlides + " Photos");
+    }
+
+
+    function destroySwiper() {
+        if (bgSwiper) {
+          bgSwiper.destroy();
+        }
+    }
+
+    let isScreenSmall = $(window).width() <= 991;
+
+    function executeOnScreenResize() {
+    const currentIsScreenSmall = $(window).width() <= 991;
+
+    if (currentIsScreenSmall !== isScreenSmall) {
+        // Эта часть кода выполнится только, если состояние экрана изменилось.
+        if (currentIsScreenSmall) {
+            destroySwiper();
+            countSlides();
+            console.log("1");
+        } else {
+            initGallery();
+            console.log("2");
+        }
+
+        // Обновляем состояние экрана
+        isScreenSmall = currentIsScreenSmall;
+    }
+    }
+
+    // Вызываем функцию при загрузке страницы и при изменении размера окна
+    $(document).ready(function() {
+    executeOnScreenResize();
+    
+    $(window).resize(function() {
+        executeOnScreenResize();
+    });
+    });
+});
