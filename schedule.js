bc09712a943448ad054b07f72b2f6be7ee290b3e
fvacreate <78551936+vladislavfva@users.noreply.github.com>
2023-09-28 20:36:42 +0200

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -1,134 +1,289 @@
-// Update
 $(document).ready(function () {
-  // Очищаем содержимое элемента с классом "t1--schedule-days"
-  var $scheduleDays = $(".t1--schedule-days").empty();
+  var $daysContainer = $(".t1--schedule-days").empty();
   var currentDate = new Date();
-  
-  // Создаем слайды для 14 дней
+
   for (var i = 0; i < 14; i++) {
-    var date = new Date(currentDate);
-    date.setDate(currentDate.getDate() + i);
-    
-    if (date.getDay() >= 1 && date.getDay() <= 5) {
-      var dayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"][date.getDay()];
-      var monthName = [
-        "January", "February", "March", "April", "May", "June", "July", "August", 
-        "September", "October", "November", "December"
-      ][date.getMonth()];
-      
-      var $slide = $('<div class="swiper-slide t1--schedule-days-slide"><div class="t1--day-of-week">' +
-        '<div class="t1--weekday-num">' + date.getDate() + '</div>' +
-        '<div class="t1--weekday">' + dayOfWeek + '</div>' +
-        '<div class="t1--schedule-months">' + monthName + '</div>' +
-        '</div></div>');
-      
-      $scheduleDays.append($slide);
+    var currentDay = new Date(currentDate);
+    currentDay.setDate(currentDate.getDate() + i);
+
+    if (currentDay.getDay() >= 1 && currentDay.getDay() <= 5) {
+      var $dayElement = $(
+        '<div class="swiper-slide t1--schedule-days-slide"><div class="t1--day-of-week">' +
+          '<div class="t1--weekday-num">' +
+          currentDay.getDate() +
+          "</div>" +
+          '<div class="t1--weekday">' +
+          ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"][
+            currentDay.getDay()
+          ] +
+          "</div>" +
+          '<div class="t1--schedule-months">' +
+          [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+          ][currentDay.getMonth()] +
+          "</div>" +
+          "</div></div>"
+      );
+      $daysContainer.append($dayElement);
     }
   }
-  
-  // Устанавливаем начальное состояние активности для первого элемента
-  var $firstDayOfWeek = $scheduleDays.children().eq(0).children().eq(0);
-  $firstDayOfWeek.addClass("is--active");
-  
-  // Извлекаем информацию о дне недели, дате и месяце
-  var selectedDate = $firstDayOfWeek.find("div.t1--weekday-num").text();
-  var selectedDayOfWeek = $firstDayOfWeek.find("div.t1--weekday").text();
-  var selectedMonth = $firstDayOfWeek.find("div.t1--schedule-months").text();
-  
-  // Обновляем заголовок месяца и поле ввода с датой
-  $(".t1--schedule__months-title").text(selectedMonth);
-  $("#date").val(selectedDate + " " + selectedMonth + " (" + selectedDayOfWeek + ")");
-  
-  // Обработчик события для клика по дню недели
+});
+
+$(document).ready(function () {
+  $(".t1--schedule-days")
+    .children()
+    .eq(0)
+    .children()
+    .eq(0)
+    .addClass("is--active");
+  let day = $(".t1--schedule-days")
+    .children()
+    .eq(0)
+    .children()
+    .eq(0)
+    .find("div.t1--weekday-num")
+    .text();
+  let weekday = $(".t1--schedule-days")
+    .children()
+    .eq(0)
+    .children()
+    .eq(0)
+    .find("div.t1--weekday")
+    .text();
+  let months = $(".t1--schedule-days")
+    .children()
+    .eq(0)
+    .children()
+    .eq(0)
+    .find("div.t1--schedule-months")
+    .text();
+  $(".t1--schedule__months-title").text(months);
+  $("#date").val(day + " " + months + " " + "(" + weekday + ")");
+
   $(".t1--day-of-week").on("click", function () {
     $(".t1--day-of-week").removeClass("is--active");
     $(this).addClass("is--active");
-    
-    selectedDate = $(this).find("div.t1--weekday-num").text();
-    selectedDayOfWeek = $(this).find("div.t1--weekday").text();
-    selectedMonth = $(this).find("div.t1--schedule-months").text();
-    
-    $(".t1--schedule__months-title").text(selectedMonth);
-    $("#date").val(selectedDate + " " + selectedMonth + " (" + selectedDayOfWeek + ")");
+    day = $(this).find("div.t1--weekday-num").text();
+    weekday = $(this).find("div.t1--weekday").text();
+    months = $(this).find("div.t1--schedule-months").text();
+
+    $(".t1--schedule__months-title").text(months);
+    $("#date").val(day + " " + months + " " + "(" + weekday + ")");
   });
-  
-  // Инициализация Swiper для слайдера
-  new Swiper(".t1--container-schedule-slider", {
-    slidesPerView: 5,
-    speed: 200,
-    loop: false,
-    keyboard: true,
-    freeMode: false,
-    slideToClickedSlide: false,
-    navigation: {
-      nextEl: ".t1--schedule-next",
-      prevEl: ".t1--schedule-prev"
+});
+
+// Schedule Date
+const rem = 16;
+new Swiper(".t1--container-schedule-slider", {
+  slidesPerView: "auto",
+  speed: 200,
+  loop: false,
+  freeMode: false,
+  slideToClickedSlide: false,
+  navigation: {
+    nextEl: ".t1--schedule-next",
+    prevEl: ".t1--schedule-prev"
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    992: {
+      spaceBetween: rem * 0.26,
+    },
+    // when window width is >= 480px
+    240: {
+      spaceBetween: 6,
     }
-  });
-  
-  // Установка значения поля "type" на "IN PERSON"
-  $("#type").val("IN PERSON");
-  
-  // Обработчик события для кнопок переключения типа
-  $(".t1--schedule-toggle-button").on("click", function () {
-    var type = $(this).text().toUpperCase();
-    $(".t1--schedule-toggle-button").removeClass("is--active");
-    $(this).addClass("is--active");
-    $("#type").val(type);
-  });
-  
-  // Определение времени до и после полудня
-  const timesBefore = ["1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM"];
-  const timesAfter = ["4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 PM"];
-  
-  // Функция для проверки времени
-  function checkTime() {
-    var currentTime = new Date().toLocaleTimeString("en-US", {
-      timeZone: "America/New_York",
-      hour12: true,
-      hour: "numeric"
-    });
-    
-    // Остальной код для проверки времени
   }
-  
-  // Вызываем функцию для проверки времени
-  checkTime();
-  
-  // Инициализация Swiper для слайдера времени
-  var timeSwiper = new Swiper(".t1--container-schedule-slider-time", {
-    slidesPerView: 5,
-    speed: 200,
-    loop: false,
-    keyboard: true,
-    freeMode: false,
-    slideToClickedSlide: false,
-    navigation: {
-      nextEl: ".t1--schedule-time-next",
-      prevEl: ".t1--schedule-time-prev"
-    }
-  });
-  
-  // Установка значения поля "home" из текста элемента с id "home-name"
-  var homeName = $("#home-name").text();
-  $("#home").val(homeName);
-  
-  // Обработчик события для выбора времени
-  $(".t1--time").on("click", function () {
-    $(".t1--time, .t1--time-type").removeClass("is--active");
-    $(this).addClass("is--active");
-    
-    var timeText = $(this).find("div.t1--time-text").text();
-    var timeType = timeText.slice(-2);
-    
-    $("#time").val(timeText);
-    
-    if (timeType === "AM") {
+});
+
+// Form Toggle
+$(document).ready(function () {
+  $("#type").val("IN PERSON");
+});
+
+$(".t1--schedule-toggle-button").on("click", function () {
+  let text = $(this).text().toUpperCase();
+  $(".t1--schedule-toggle-button").removeClass("is--active");
+  $(this).addClass("is--active");
+  $("#type").val(text);
+});
+
+const timesBefore = [
+  "1 AM",
+  "2 AM",
+  "3 AM",
+  "4 AM",
+  "5 AM",
+  "6 AM",
+  "7 AM",
+  "8 AM"
+];
+
+const timesAfter = [
+  "4 PM",
+  "5 PM",
+  "6 PM",
+  "7 PM",
+  "8 PM",
+  "9 PM",
+  "10 PM",
+  "11 PM",
+  "12 PM"
+];
+
+function checkTime() {
+  const timeVersion2 = {
+    timeZone: "America/New_York",
+    hour12: true,
+    hour: "numeric"
+  };
+
+  let currentTime = new Date().toLocaleTimeString("en-US", timeVersion2);
+  const currentDay = $(".t1--schedule-days")
+    .children()
+    .children()
+    .eq(0)
+    .eq(0)
+    .find("div.t1--weekday-num")
+    .text();
+  let day = currentDay;
+
+  function timeActive() {
+    $(".t1--time").removeClass("is--active");
+    $(".t1--schedule-time")
+      .children()
+      .filter(":not(.is--disabled)")
+      .first()
+      .children()
+      .eq(0)
+      .addClass("is--active");
+    let timeType = $(".t1--time.is--active")
+      .find("div.t1--time-type-text")
+      .text();
+    let timeChecker = timeType.slice(-2);
+    $("#time").val(timeType);
+
+    if (timeChecker === "AM") {
       $(".t1--time-type").removeClass("is--active");
       $(".t1--time-type__wrapper").children().eq(0).addClass("is--active");
     } else {
       $(".t1--time-type").removeClass("is--active");
       $(".t1--time-type__wrapper").children().eq(1).addClass("is--active");
     }
+
+    let count = $(".swiper-slide.is--disabled").length;
+    console.log(count);
+
+    if (count >= 9) {
+      $(".t1--schedule-slider-time").addClass("is--disabled");
+      $(".t1--no-time").removeClass("is--disabled");
+    }
+  }
+
+  function checkDate() {
+    if (day === currentDay) {
+      if (timesAfter.includes(currentTime)) {
+        $(".t1--schedule-slider-time").addClass("is--disabled");
+        $(".t1--no-time").removeClass("is--disabled");
+        console.log("Disabled");
+      } else if (currentTime === "7 AM") {
+        $(".t1--schedule-time").children().eq(0).addClass("is--disabled");
+      } else if (currentTime === "8 AM") {
+        $(".t1--schedule-time").children().slice(0, 2).addClass("is--disabled");
+      } else {
+        $(".t1--time-type-text").each(function () {
+          let i = $(this).parent().parent().index();
+          let timeText = $(this).text();
+          if (timeText === currentTime) {
+            $(".t1--schedule-time").children().slice(i, i + 3).addClass("is--disabled");
+            $(".t1--schedule-time").children().eq(i).prevAll().addClass("is--disabled");
+          }
+          timeSwiper.update();
+        });
+      }
+    } else {
+      $(".t1--schedule-slider-time").removeClass("is--disabled");
+      $(".t1--no-time").addClass("is--disabled");
+      console.log("Enabled");
+    }
+  }
+  checkDate();
+  timeActive();
+
+  $(".t1--day-of-week").on("click", function () {
+    day = $(this).find("div.t1--weekday-num").text();
+    $(".swiper-slide").removeClass("is--disabled");
+    checkDate();
+    timeActive();
+    timeSwiper.update();
   });
+}
+
+$(document).ready(function () {
+  checkTime();
+});
+
+// Schedule Time
+var timeSwiper = new Swiper(".t1--container-schedule-slider-time", {
+  slidesPerView: "auto",
+  speed: 200,
+  loop: false,
+  freeMode: false,
+  slideToClickedSlide: false,
+  navigation: {
+    nextEl: ".t1--schedule-time-next",
+    prevEl: ".t1--schedule-time-prev"
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    992: {
+      spaceBetween: rem * 0.26,
+    },
+    // when window width is >= 480px
+    240: {
+      spaceBetween: 6,
+    }
+  }
+});
+
+// Time
+$(document).ready(function () {
+  let time = $(".t1--schedule-time")
+    .children()
+    .eq(0)
+    .children()
+    .eq(0)
+    .find("div.t1--time-text")
+    .text();
+
+  let homeName = $("#home-name").text();
+  $("#home").val(homeName);
+});
+
+$(".t1--time").on("click", function () {
+  $(".t1--time, .t1--time-type").removeClass("is--active");
+  $(this).addClass("is--active");
+  let time = $(this).find("div.t1--time-text").text();
+  let timeType = $(this).find("div.t1--time-type-text").text();
+  let timeChecker = timeType.slice(-2);
+  $("#time").val(timeType);
+
+  if (timeChecker === "AM") {
+    $(".t1--time-type").removeClass("is--active");
+    $(".t1--time-type__wrapper").children().eq(0).addClass("is--active");
+  } else {
+    $(".t1--time-type").removeClass("is--active");
+    $(".t1--time-type__wrapper").children().eq(1).addClass("is--active");
+  }
 });
