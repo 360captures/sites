9f7f417afbab7c4d35cde169c14ea4c71e06597b
fvacreate <78551936+vladislavfva@users.noreply.github.com>
2023-09-28 20:36:16 +0200

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -6,8 +6,13 @@ function initSwiper() {
     $('.t1--slider__content').each(function(index) {
       bgSwiper = new Swiper($(this).find('.swiper.t1--gallery__slider')[0], {
         slidesPerView: "auto",
-        speed: 1000,
+        speed: 300,
+        effect: "fade",
+        crossFade: true,
         allowTouchMove: false,
+        mousewheel: {
+            forceToAxis: true,
+        }
       });
     });
 }
@@ -16,16 +21,16 @@ function initThumbs() {
     $('.t1--slider__content').each(function(index) {
         thumbsSwiper = new Swiper($(this).find('.swiper.t1--gallery__thumbs')[0], {
             allowTouchMove: true,
-            speed: 1000,
+            speed: 300,
             slidesPerView: 'auto',
             slideToClickedSlide: true,
             centeredSlides: true,
             spaceBetween: 16,
             mousewheel: {
-            forceToAxis: true,
+                forceToAxis: true,
             },
             thumbs: {
-            swiper: bgSwiper,
+                swiper: bgSwiper,
             },
             navigation: {
             nextEl: '.t1--gallery__next',
@@ -35,56 +40,87 @@ function initThumbs() {
     });
 }
 
-$(document).ready(function(){
-    function initGallery() {
-        initSwiper();
-        initThumbs();
-    }
-
-    if ($(window).width() > 991) {
-        initGallery();
-    }
-
-    function countSlides() {
-        let totalSlides = $('.t1--slider__content').find('.swiper-slide.t1--gallery__image').length;
-        $('.t1--open-lightbox').text("View All " + totalSlides + " Photos");
-    }
-
-    if ($(window).width() <= 991) {
-        countSlides();
-    }
-
-    function destroySwiper() {
-        if (bgSwiper) {
-          bgSwiper.destroy();
+function initGalleries(){
+    $(document).ready(function(){
+        function initGallery() {
+            initSwiper();
+            initThumbs();
         }
-    }
-
-    let isScreenSmall = $(window).width() <= 991;
-
-    function executeOnScreenResize() {
-    const currentIsScreenSmall = $(window).width() <= 991;
-
-    if (currentIsScreenSmall !== isScreenSmall) {
-        // Эта часть кода выполнится только, если состояние экрана изменилось.
-        if (currentIsScreenSmall) {
-            destroySwiper();
-            countSlides();
-        } else {
+    
+        if ($(window).width() > 991) {
             initGallery();
         }
-
-        // Обновляем состояние экрана
-        isScreenSmall = currentIsScreenSmall;
-    }
-    }
-
-    // Вызываем функцию при загрузке страницы и при изменении размера окна
-    $(document).ready(function() {
-    executeOnScreenResize();
     
-    $(window).resize(function() {
+        function countSlides() {
+            let totalSlides = $('.t1--slider__content').find('.swiper-slide.t1--gallery__image').length;
+            $('.t1--open-lightbox').text("View All " + totalSlides + " Photos");
+        }
+    
+        if ($(window).width() <= 991) {
+            countSlides();
+        }
+    
+        function destroySwiper() {
+            if (bgSwiper) {
+              bgSwiper.destroy();
+            }
+        }
+    
+        let isScreenSmall = $(window).width() <= 991;
+    
+        function executeOnScreenResize() {
+        const currentIsScreenSmall = $(window).width() <= 991;
+    
+        if (currentIsScreenSmall !== isScreenSmall) {
+            // Эта часть кода выполнится только, если состояние экрана изменилось.
+            if (currentIsScreenSmall) {
+                destroySwiper();
+                countSlides();
+            } else {
+                initGallery();
+            }
+    
+            // Обновляем состояние экрана
+            isScreenSmall = currentIsScreenSmall;
+        }
+        }
+    
+        // Вызываем функцию при загрузке страницы и при изменении размера окна
+        $(document).ready(function() {
         executeOnScreenResize();
+        
+        $(window).resize(function() {
+            executeOnScreenResize();
+        });
+        });
     });
+}
+
+var cmsList = document.getElementById("cms-list");
+var cmsList2 = document.getElementById("cms-list2");
+
+var changesCompleted = false; // Флаг для отслеживания завершения изменений
+
+function observeChanges(element, callback) {
+  var observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+      if (mutation.type === 'childList') {
+        if (!changesCompleted) {
+          changesCompleted = true; // Устанавливаем флаг в true после первого вызова
+          callback();
+        }
+      }
     });
-});
+  });
+
+  observer.observe(element, { childList: true, subtree: true });
+}
+
+// Следим за изменениями
+observeChanges(cmsList, myCode);
+observeChanges(cmsList2, myCode);
+
+// Функция, которую нужно выполнить после завершения всех изменений
+function myCode() {
+  initGalleries();
+}
